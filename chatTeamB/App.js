5f61cf40ad1bfbe1723d83dc77c71702acd7170b
Candelaria Olivia Espeche <candelaria.espeche@globant.com>
2020-12-01 14:19:39 -0300

    first commit

---
+++ chatTeamB/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Chat Grupo B</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
