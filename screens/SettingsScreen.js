eb754436ddde706be5bc5b1ac5b854484e053d23
Candelaria Olivia Espeche <candelaria.espeche@globant.com>
2020-12-04 09:08:04 -0300

    Dark Mode with useContext

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -2,8 +2,25 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Colors, DARKMODE} from '../utils/Colors';
 import SettingOption from '../components/SettingOption';
+import {useTheme} from '../utils/ThemeContext';
 
 const SettingsScreen = (props) => {
+  const {colors, isDark} = useTheme();
+
+  const styles = {
+    screen: {
+      flex: 1,
+      backgroundColor: colors.background,
+      alignItems: 'center',
+    },
+    hr: {
+      height: 0.5,
+      width: '100%',
+      backgroundColor: DARKMODE
+        ? Colors.lightMode.darkGrey
+        : Colors.lightMode.grey,
+    },
+  };
   return (
     <View style={styles.screen}>
       <SettingOption
@@ -39,19 +56,19 @@ const SettingsScreen = (props) => {
   );
 };
 
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    backgroundColor: DARKMODE ? Colors.darkMode.darkGrey : 'white',
-    alignItems: 'center',
-  },
-  hr: {
-    height: 0.5,
-    width: '100%',
-    backgroundColor: DARKMODE
-      ? Colors.lightMode.darkGrey
-      : Colors.lightMode.grey,
-  },
-});
+// const styles = StyleSheet.create({
+//   screen: {
+//     flex: 1,
+//     backgroundColor: DARKMODE ? Colors.darkMode.darkGrey : 'white',
+//     alignItems: 'center',
+//   },
+//   hr: {
+//     height: 0.5,
+//     width: '100%',
+//     backgroundColor: DARKMODE
+//       ? Colors.lightMode.darkGrey
+//       : Colors.lightMode.grey,
+//   },
+// });
 
 export default SettingsScreen;
