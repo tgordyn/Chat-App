0ff130bc6dbbc6a1cf579cd56c23e671439e64ee
Tomas Gordyn <tomas.gordyn@globant.com>
2020-12-14 17:40:29 -0300

    avances adaptando estilos a chatScreen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,66 +1,98 @@
-
-import React, { useState, useEffect, useRef}  from "react";
-import { TextInput, FlatList, StyleSheet, Text, View } from "react-native";
-import io from "socket.io-client";
+import React, {useState, useEffect, useRef} from 'react';
+import {TextInput, FlatList, StyleSheet, Text, View} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import io from 'socket.io-client';
 import ChatHeader from '../components/Chat/ChatHeader';
+import ChatItem from "../components/Chat/ChatItem";
+import TypeMessage from "../components/Chat/TypeMessage.js";
+import {Colors, DARKMODE} from '../utils/Colors';
+import {useTheme} from '../utils/ThemeContext';
 
 let socket;
-const ChatScreen = ()=>{
-  const [chatMessage, setChatMessage]= useState("");
-  const [chatMessages, setChatMessages]= useState([]);
-const ENDPOINT = "ws://192.168.0.11:3000"
+const ChatScreen = () => {
+  const {colors, isDark} = useTheme();
+  const [chatMessage, setChatMessage] = useState('');
+  const [chatMessages, setChatMessages] = useState([]);
+  const [contador, setContador] = useState(0)
+  const ENDPOINT = 'ws://192.168.0.11:3000';
 
-useEffect(() => {
-  socket = io(ENDPOINT)
-}, [ENDPOINT]);
+  const styles = {
+    screen: {
+      flex: 1,
+      backgroundColor: colors.backgroundChat,
+    },
+  };
 
-useEffect(() => {
-  socket.on('chat message', (msg) => {
-    console.log(msg);
-    setChatMessages([...chatMessages, msg]);
-  });
-  console.log("array", chatMessages)
-}, [chatMessages]);
+  useEffect(() => {
+    socket = io(ENDPOINT);
+  }, [ENDPOINT]);
 
-  const submitChatMessage= ()=> {
+  useEffect(() => {
+    socket.on('chat message', (msg) => {
+      //console.log(msg);
+      console.log(contador + 1);
+      setContador(contador + 1);
+      //console.log(contador);
+      setChatMessages([...chatMessages, msg]);
+    });
+    console.log('array', chatMessages);
+  }, [chatMessages]);
 
-    socket.emit("chat message", chatMessage);
-    setChatMessage( "" );
-  }
+  const submitChatMessage = () => {
+    socket.emit('chat message', chatMessage);
+    setChatMessage('');
+  };
 
-  const chatMsgs =  chatMessages.map(chatM => (
-    <Text key={chatM}>{chatM}</Text>
-  ))
+  const chatMsgs = chatMessages.map((chatM, index) => (
+    // <Text key={index + chatM}>{chatM}</Text>
+    <ChatItem 
+    key={index + chatM}
+    index={index}
+    message={chatM}
+    />
+  ));
 
   return (
-    <View style={styles.container}>
-        <ChatHeader />
-        
-        <TextInput
-          style={{ height: 40, borderWidth: 2, justifyContent:"flex-end" }}
-          autoCorrect={false}
-          value={chatMessage}
-          onSubmitEditing={() => submitChatMessage()}
-          onChangeText={chatMessage => {
-            setChatMessage( chatMessage );
-          }}
-        />
-                {chatMsgs}
-      </View>
-  )
-}
+    <View style={styles.screen}>
+      <ChatHeader />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F5FCFF"
-  },
-});
+      <TextInput
+        style={{height: 40, borderWidth: 2, justifyContent: 'flex-end'}}
+        autoCorrect={false}
+        value={chatMessage}
+        onSubmitEditing={() => submitChatMessage()}
+        onChangeText={(chatMessage) => {
+          setChatMessage(chatMessage);
+        }}
+      />
+      <ScrollView>
+       {chatMsgs}
+      </ScrollView>
+      <TypeMessage 
+      // autoCorrect={false}
+      // value={chatMessage}
+      // onSubmitEditing={() => submitChatMessage()}
+      // onChangeText={(chatMessage) => {
+      //   setChatMessage(chatMessage);
+      // }}
+      />
+    </View>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#F5FCFF',
+//   },
+//   screen: {
+//     flex: 1,
+//     backgroundColor: DARKMODE
+//       ? Colors.darkMode.darkGrey
+//       : Colors.lightMode.lightGrey,
+//   },
+// });
 export default ChatScreen;
-
-
-
-
 
 ///////////////////////// prueba componente de clase ////////////////////
 // import React, { Component, useState, useEffect}  from "react";
@@ -80,7 +112,7 @@ export default ChatScreen;
 //   }
 
 //   componentDidMount() {
-    
+
 //     this.socket = io("http://192.168.0.11:3000");
 //     this.socket.on("chat message", msg => {
 //       this.setState({ chatMessages: [...this.state.chatMessages, msg] });
@@ -100,7 +132,7 @@ export default ChatScreen;
 //     return (
 //       <View style={styles.container}>
 //         <ChatHeader />
-        
+
 //         <TextInput
 //           style={{ height: 40, borderWidth: 2, justifyContent:"flex-end" }}
 //           autoCorrect={false}
