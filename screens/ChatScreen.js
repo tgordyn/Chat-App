b17072228ba71ad542d1f67a2fff7aaf3b2bcd24
Candelaria Olivia Espeche <candelaria.espeche@globant.com>
2020-12-15 15:49:42 -0300

    Added hour of message in ChatItem

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -5,12 +5,15 @@ import ChatHeader from '../components/Chat/ChatHeader';
 import ChatItem from '../components/Chat/ChatItem';
 import TypeMessage from '../components/Chat/TypeMessage.js';
 import {useTheme} from '../utils/ThemeContext';
+// import {load} from '../utils/AsyncStorage';
 
 const ChatScreen = () => {
   const {colors, isDark} = useTheme();
   const [chatMessage, setChatMessage] = useState('');
   const [chatMessages, setChatMessages] = useState([]);
   const [user, setUser] = useState(null);
+  // const [data, setData] = useState(null);
+  // const socket = io(data ? `ws://${data.ip}:${data.port}` : null);
   const socket = io('ws://192.168.0.8:3000');
   socket.connect();
 
@@ -35,6 +38,10 @@ const ChatScreen = () => {
     return () => cleanUp();
   }, []);
 
+  // useEffect(() => {
+  //   load(setData);
+  // }, [data]);
+
   useEffect(() => {
     socket.on('chat message', (msg) => {
       setChatMessages([...chatMessages, msg]);
@@ -43,7 +50,14 @@ const ChatScreen = () => {
   }, [chatMessages]);
 
   const submitChatMessage = () => {
-    socket.emit('chat message', {chatMessage, user});
+    var hours = new Date();
+    var minutes =
+      hours.getMinutes() <= 9 ? `0${hours.getMinutes()}` : hours.getMinutes();
+    socket.emit('chat message', {
+      chatMessage,
+      user,
+      hour: `${hours.getHours()}:${minutes}`,
+    });
     setChatMessage('');
   };
   const handleOnChange = (text) => {
@@ -55,7 +69,11 @@ const ChatScreen = () => {
   return (
     <View style={styles.screen}>
       <ChatHeader />
-      <FlatList data={chatMessages} renderItem={renderItem} />
+      <FlatList
+        data={chatMessages}
+        renderItem={renderItem}
+        keyExtractor={(data, index) => data + index}
+      />
       <TypeMessage
         submitChatMessage={submitChatMessage}
         handleOnChange={handleOnChange}
